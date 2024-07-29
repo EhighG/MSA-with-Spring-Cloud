3a91a9c7fb81e8a6f5e712db422bbf7cf8369a4c
Ehigh <rkd2rb@naver.com>
2024-07-30 00:03:03 +0900

    Init: api gateway 추가, 연동
    - 기존 실습 코드에서 복사
    - Service Discovery, User Service와 연동

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UserController {
 
     private Environment env;
@@ -30,7 +30,8 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return "It's working in User Service.";
+        return String.format("It's working in User Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
